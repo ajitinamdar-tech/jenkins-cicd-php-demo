d0c4cad3278f0ecdfea475d05e420ba577d0601b
Ajit Inamdar <88879506+ajitinamdar-tech@users.noreply.github.com>
2022-06-27 23:35:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Index</title>
  </head>
  <body>
- <?php echo '<p>Hello world from PHP! Declarative pipeline!</p>'; ?> 
+ <?php echo '<p>Hello world from PHP! Freestyle project!</p>'; ?> 
  </body>
 </html>
