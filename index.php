02b2edb33504a5b80f8bb0bff386c6bbdd491084
Bala0911 <134393829+Bala0911@users.noreply.github.com>
2023-05-26 16:01:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Index</title>
  </head>
  <body>
- <?php echo '<p>Hello world from PHP! Declarative pipeline script</p>'; ?> 
+ <?php echo '<p>Hello world from PHP! Declarative pipeline</p>'; ?> 
  </body>
 </html>
