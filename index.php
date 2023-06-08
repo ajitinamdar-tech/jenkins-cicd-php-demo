e9f39419926448d63b74d0bb1a7e4dc2bf5f8263
moula-shaik <115640750+moula-shaik@users.noreply.github.com>
2023-06-08 17:15:15 +0530

    Update index.php
    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Index</title>
  </head>
  <body>
- <?php echo '<p>Hello world from PHP! Declarative pipeline!! apkapay </p>'; ?> 
+ <?php echo '<p>Hello world from PHP! Declarative pipeline!! Welcome to Apkapay </p>'; ?> 
  </body>
 </html>
